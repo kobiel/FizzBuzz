1f3bde37e62b38155cb4e7260e2d272bbffe0527
Kobi <kobieliasi@gmail.com>
2014-04-22 12:48:40 +0300

    REFACTOR: global var and new test method by ahuva

--- FizzBuzzTest/UnitTest.cs
+++ FizzBuzzTest/UnitTest.cs
@@ -7,12 +7,27 @@ namespace FizzBuzz
     [TestClass]
     public class UnitTest
     {
-        [TestMethod]
-        public void TestMethod()
+        private Prog Program = new Prog();
+        public Prog program
         {
-            var test = new Prog();
+            get { return Program; }
+            set { Program = value; }
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
             int expect = 1;
-            int actual = test.FizzBuzz(1);
+            int actual = Program.FizzBuzz(1);
+            Assert.AreEqual<int>(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int expect = 2;
+            int actual = Program.FizzBuzz(2);
             Assert.AreEqual<int>(expect, actual);
         }
     }
