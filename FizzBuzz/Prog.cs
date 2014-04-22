f6ea0f81610d96be2e368199226c54cc499fab31
Kobi <kobieliasi@gmail.com>
2014-04-22 12:14:27 +0300

    init

---
+++ FizzBuzz/Prog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTest
+{
+    class Prog
+    {
+        static void main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ FizzBuzzTest/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.Fail();
+        }
+    }
+}
